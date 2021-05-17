dfc32bd0696c4f4c2c7230f6d647a9660a1fa8fe
Thiago Vieira Pereira <th91vi@gmail.com>
2021-05-17 12:26:47 -0300

    feat: added basic pages and routing

---
+++ music-events-frontend/pages/about.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, eveniet
+        ut? Esse, reprehenderit amet nisi reiciendis consequuntur qui voluptatum
+        autem id illo earum voluptatem quaerat. Nemo aliquam vel suscipit quam.
+      </p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ music-events-frontend/pages/events/addEvent.jsx
@@ -0,0 +1,9 @@
+const AddEventPage = () => {
+  return (
+    <div>
+      <h1>Add event</h1>
+    </div>
+  );
+};
+
+export default AddEventPage;
