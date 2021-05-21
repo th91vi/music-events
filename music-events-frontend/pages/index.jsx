ab03af97183238817266502bc3146a9609bf71c3
Thiago Vieira Pereira <th91vi@gmail.com>
2021-05-21 13:02:52 -0300

    feat: added config file with base URl. added server sie props to home page

--- music-events-frontend/pages/index.jsx
+++ music-events-frontend/pages/index.jsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-const Home = () => {
+const Home = ({ events }) => {
   return (
     <Layout>
-      <Link href="/about">About</Link>
+      <Link href="/about">Upcoming events</Link>
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
+}
 
 export default Home;
