861487020be444613b02e3e110f99356c5d149cf
Thiago Vieira Pereira <th91vi@gmail.com>
2021-05-31 13:26:54 -0300

    feat: singlepage event markup, styles

--- music-events-frontend/pages/events/[slug].jsx
+++ music-events-frontend/pages/events/[slug].jsx
@@ -1,10 +1,52 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FaPencilAlt, FaTimes, FaAngleDoubleLeft } from "react-icons/fa";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 
+import * as S from "./Slug.styles";
+
 const EventPage = ({ evt }) => {
+  const deleteEvent = (e) => {
+    console.log("e");
+  };
+
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <S.EventsSlug>
+        <div className="event">
+          <div className="controls">
+            <Link href={`/events/edit/${evt.id}`}>
+              <a>
+                <FaPencilAlt /> Edit Event
+              </a>
+            </Link>
+            <a href="#" className="delete" onClick={deleteEvent}>
+              <FaTimes /> Delete Event
+            </a>
+          </div>
+          <span>
+            {evt.date} at {evt.time}
+          </span>
+          <h1>{evt.name}</h1>
+          {evt.image && (
+            <div className="image">
+              <Image src={evt.image} width={960} height={600} />
+            </div>
+          )}
+          <h3>Performers:</h3>
+          <p>{evt.performers}</p>
+          <h3>Description:</h3>
+          <p>{evt.description}</p>
+          <h3>Venue: {evt.venue}</h3>
+          <p>{evt.address}</p>
+          <Link href="/events">
+            <a className="back">
+              <FaAngleDoubleLeft /> Go back
+            </a>
+          </Link>
+        </div>
+      </S.EventsSlug>
     </Layout>
   );
 };
