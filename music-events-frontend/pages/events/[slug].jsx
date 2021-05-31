c6620066e13d7aca4e901c190cd0487c5c648eac
Thiago Vieira Pereira <th91vi@gmail.com>
2021-05-31 13:02:52 -0300

    feat: added getServerSideProps to events slug

--- music-events-frontend/pages/events/[slug].jsx
+++ music-events-frontend/pages/events/[slug].jsx
@@ -1,11 +1,21 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-const EventPage = () => {
+const EventPage = ({ evt }) => {
   return (
     <Layout>
-      <h1>My event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
   );
 };
+
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: { evt: events[0] },
+  };
+}
 
 export default EventPage;
