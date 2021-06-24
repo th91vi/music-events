9a555f9d35ee75e32cd35a213ddb34cd61930bff
Thiago Vieira Pereira <th91vi@gmail.com>
2021-06-24 13:34:00 -0300

    feat: fetch user events for dashboard

--- music-events-frontend/pages/account/dashboard.js
+++ music-events-frontend/pages/account/dashboard.js
@@ -1,11 +1,31 @@
+import { parseCookies } from "@/helpers/index";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-const DashboardPage = () => {
+const DashboardPage = ({events}) => {
+  console.log(events)
   return (
     <Layout title="User dashboard">
       <h1>Dashboard</h1>
     </Layout>
   );
 };
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
+}
 
 export default DashboardPage;
